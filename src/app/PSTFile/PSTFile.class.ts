b3870c1a101b7a188ffb8ae82b311a5048fc784f
Ed Pfromer <epfromer@gmail.com>
2018-02-11 18:39:50 -0700

    working on PSTfile

--- src/app/PSTFile/PSTFile.class.ts
+++ src/app/PSTFile/PSTFile.class.ts
@@ -3,10 +3,10 @@ import * as fs from 'fs';
 export class PSTFile {
     public static ENCRYPTION_TYPE_NONE: number = 0;
     public static ENCRYPTION_TYPE_COMPRESSIBLE: number = 1;
-    static MESSAGE_STORE_DESCRIPTOR_IDENTIFIER: number = 33;
-    static ROOT_FOLDER_DESCRIPTOR_IDENTIFIER: number = 290;
+    public static MESSAGE_STORE_DESCRIPTOR_IDENTIFIER: number = 33;
+    public static ROOT_FOLDER_DESCRIPTOR_IDENTIFIER: number = 290;
     public static PST_TYPE_ANSI: number = 14;
-    static PST_TYPE_ANSI_2: number = 15;
+    public static PST_TYPE_ANSI_2: number = 15;
     public static PST_TYPE_UNICODE: number = 23;
     public static PST_TYPE_2013_UNICODE: number = 36;
     public static PS_PUBLIC_STRINGS: number = 0;
@@ -25,20 +25,50 @@ export class PSTFile {
     public static PSETID_AirSync: number = 13;
     public static PSETID_Sharing: number = 14;
 
+    private _pstFileType = 0;
+
     public constructor(fileName: string) {
         // attempt to open file
         // confirm first 4 bytes are !BDN
-        let data = '';
-        let stream = fs.createReadStream(fileName, { start: 0, end: 3 });
+        let stream = fs.createReadStream(fileName, { start: 0, end: 514 });
         stream.on('error', function() {
             throw new Error('Error opening ' + fileName);
         });
         stream.on('data', function(chunk) {
-            if (chunk != '!BDN') {
-                throw new Error(
-                    'Invalid file header: ' + chunk + ', expected: !BDN'
-                );
+            let key = '!BDN';
+            if (chunk[0] != key.charCodeAt(0) ||
+                chunk[1] != key.charCodeAt(1) ||
+                chunk[2] != key.charCodeAt(2) ||
+                chunk[3] != key.charCodeAt(3)
+            ) {
+                throw new Error('Invalid file header (expected: "!BDN"): ' + chunk);
+            }
+
+            // make sure we are using a supported version of a PST...
+            if (chunk[10] ===  PSTFile.PST_TYPE_ANSI_2) {
+                chunk[10] = PSTFile.PST_TYPE_ANSI
+            }
+            if (chunk[10] !== PSTFile.PST_TYPE_ANSI &&
+                chunk[10] !== PSTFile.PST_TYPE_UNICODE &&
+                chunk[10] !== PSTFile.PST_TYPE_2013_UNICODE
+            ) {
+                throw new Error('Unrecognised PST File version: ' + chunk[10]);
+            }
+            this._pstFileType = chunk[10];
+
+            // make sure no encryption
+            let encryptionType: number;
+            if (this._pstFileType === PSTFile.PST_TYPE_ANSI) {
+                encryptionType = chunk[461];
+            } else {
+                encryptionType = chunk[513];
+            }
+            if (encryptionType === 0x02) {
+                throw new Error('PST is encrypted');
             }
         });
+
     }
+
+    get pstFileType(): number { return this._pstFileType; }
 }
