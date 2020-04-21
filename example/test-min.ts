58ffb81fe7d7e35bdebf660312c8c7cba28afff9
Ed Pfromer <epfromer@gmail.com>
2020-04-21 12:13:20 -0600

    updating

---
+++ example/test-min.ts
@@ -0,0 +1,68 @@
+import { PSTMessage } from '../src/PSTMessage/PSTMessage.class'
+import { PSTFile } from '../src/PSTFile/PSTFile.class'
+import { PSTFolder } from '../src/PSTFolder/PSTFolder.class'
+const resolve = require('path').resolve
+
+let depth = -1
+let col = 0
+
+const pstFile = new PSTFile(resolve('./testdata/enron.pst'))
+console.log(pstFile.getMessageStore().displayName)
+processFolder(pstFile.getRootFolder())
+
+/**
+ * Walk the folder tree recursively and process emails.
+ * @param {PSTFolder} folder
+ */
+function processFolder(folder: PSTFolder) {
+  depth++
+
+  // the root folder doesn't have a display name
+  if (depth > 0) {
+    console.log(getDepth(depth) + folder.displayName)
+  }
+
+  // go through the folders...
+  if (folder.hasSubfolders) {
+    let childFolders: PSTFolder[] = folder.getSubFolders()
+    for (let childFolder of childFolders) {
+      processFolder(childFolder)
+    }
+  }
+
+  // and now the emails for this folder
+  if (folder.contentCount > 0) {
+    depth++
+    let email: PSTMessage = folder.getNextChild()
+    while (email != null) {
+      console.log(
+        getDepth(depth) +
+          'Sender: ' +
+          email.senderName +
+          ', Subject: ' +
+          email.subject
+      )
+      email = folder.getNextChild()
+    }
+    depth--
+  }
+  depth--
+}
+
+/**
+ * Returns a string with visual indication of depth in tree.
+ * @param {number} depth
+ * @returns {string}
+ */
+function getDepth(depth: number): string {
+  let sdepth = ''
+  if (col > 0) {
+    col = 0
+    sdepth += '\n'
+  }
+  for (let x = 0; x < depth - 1; x++) {
+    sdepth += ' | '
+  }
+  sdepth += ' |- '
+  return sdepth
+}
